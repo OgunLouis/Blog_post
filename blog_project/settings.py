d9b2e0cb393cc321fcf66f4302844929303fffab
OgunLouis <ogunnowolouis@gmail.com>
2025-07-17 13:34:57 +0100

    Added new features

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from imagekitio import ImageKit
 from decouple import config
 import dj_database_url
 import os
